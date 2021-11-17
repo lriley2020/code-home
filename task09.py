12783cc5cef56ff33dd98a8b12579b6c55150aaa
lriley2020 <liam.j.06.riley@gmail.com>
2021-11-17 15:26:26 +0000

    Added extension to homework

--- task09.py
+++ task09.py
@@ -1,10 +1,17 @@
 peopleandhobbies = {}
+approved_hobbies = ["skateboarding", "cycling", "saxaphone"]
 
 while True:
   try:
     print("Enter an name followed by a hobby:")
     name = input("> ")
-    hobby = input("> ")
+    hobby = ""
+    while hobby not in approved_hobbies:
+      print("Enter a hobby  in this list:")
+      print(approved_hobbies)
+      hobby = input("> ").lower()
+      if hobby not in approved_hobbies:
+        print("Enter a hobby that is in the list and try again")
     peopleandhobbies[name] = hobby
   except KeyboardInterrupt:
     print("Done")
