c3991b882fa51a8e25e6c64873e49b3faf0f806c
lriley2020 <liam.j.06.riley@gmail.com>
2021-11-24 19:07:02 +0000

    Added support for invalid menu options

--- task10.py
+++ task10.py
@@ -7,11 +7,8 @@ odds = 0
 while True:
     option = input("option> ")
     if option == "add numbers":
-        try:
-            inputted = int(input("number> "))
-            evens, odds = (evens + 1, odds) if inputted % 2 == 0 else (evens, odds + 1)
-
-        except KeyboardInterrupt:
-            break
+        inputted = int(input("number> "))
+        evens, odds = (evens + 1, odds) if inputted % 2 == 0 else (evens, odds + 1)
     if option == "quit": exit()
     if option == "see numbers": print(f"There were {evens} evens and {odds} odds")
+    else: print("Oops, that's not a valid option!")
