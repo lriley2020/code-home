0eb58e69243850d997070e2a8df2e0358b0e8024
lriley2020 <liam.j.06.riley@gmail.com>
2022-02-09 19:44:29 +0000

    Added loop

--- task16.5.py
+++ task16.5.py
@@ -8,9 +8,12 @@ with open("namegame.db", "r") as f:
 vowels = ["A", "E", "I", "O", "O"]
 if name[0] not in vowels: newname = name[1:]
 else: newname = name
-print(f"""{name}, {name}, bo{'-b' if name[0] != 'B' else '-'}{newname.lower()},
-Bonana-fanna fo{'-f' if name[0] != 'F' else '-'}{newname.lower()},
-Fee fi mo-m{'-m' if name[0] != 'M' else '-'}{newname.lower()},
-{name}!""")
+again = "y"
+while again == "y":
+    print(f"""{name}, {name}, bo{'-b' if name[0] != 'B' else '-'}{newname.lower()},
+    Bonana-fanna fo{'-f' if name[0] != 'F' else '-'}{newname.lower()},
+    Fee fi mo-m{'-m' if name[0] != 'M' else '-'}{newname.lower()},
+    {name}!""")
+    again = input("again?(y/n)> ")
 with open("namegame.db", "a") as f:
     f.write(f"{name}\n")
